f31f91ac0f906554b10bc4d7de8ac93ec96235ca
James Scott <scottj1426@gmail.com>
2018-03-14 10:03:12 -0500

    another one done

--- toyProblems.js
+++ toyProblems.js
@@ -1,11 +1,9 @@
-
 // Simple, remove the spaces from the string, then return the resultant string.
-function noSpace(x){
-    x = x.replace(/\s/g,'');
-    return x;
-    }
+function noSpace(x) {
+  x = x.replace(/\s/g, "");
+  return x;
+}
 
-    
 //     //Leo's girlfriend asked him to buy a gift list during his next trip, now he wants to know how many of them will he be able to buy.
 
 // Write the following function to help Leo out:
@@ -20,21 +18,27 @@ function noSpace(x){
 
 // NOTE: All numbers will be integers >= 0, and the array will never be empty.
 
-function howManyGifts (maxBudget, gifts) {
-    var count = 0;
-    var sum = 0;
-    
-    gifts.sort(function(a, b) {
-        return a - b;
-    });
+function howManyGifts(maxBudget, gifts) {
+  var count = 0;
+  var sum = 0;
 
-    for (var i = 0; i < gifts.length; i++) {
-        sum = sum + gifts[i];
-        
-        if (sum <= maxBudget){
-            count = count + 1;
-        }
+  gifts.sort(function(a, b) {
+    return a - b;
+  });
+
+  for (var i = 0; i < gifts.length; i++) {
+    sum = sum + gifts[i];
+
+    if (sum <= maxBudget) {
+      count = count + 1;
     }
-    
-    return count;
+  }
+
+  return count;
 }
+
+// You were camping with your friends far away from home, but when it's time to go back, you realize that you fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. Function should return true if it is possible and false if not.
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    // TODO
+    return distanceToPump/mpg <= fuelLeft;
+  };
