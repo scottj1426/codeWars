d7d64285e0f7093ff78fa880a97476cb713a30c7
James Scott <scottj1426@gmail.com>
2018-04-02 10:33:49 -0500

    another problem down

--- toyProblems.js
+++ toyProblems.js
@@ -77,3 +77,47 @@ return list
 .filter(dev => dev.language === "JavaScript")
 .filter(dev => dev.continent === "Europe")
 .length
+
+// Given an array of strings, return another array containing all of its longest strings.
+
+// Example
+
+// For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+// allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
+
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] array.string inputArray
+
+// A non-empty array.
+
+// Guaranteed constraints:
+// 1 ≤ inputArray.length ≤ 10,
+// 1 ≤ inputArray[i].length ≤ 10.
+
+// [output] array.string
+
+// Array of the longest strings, stored in the same order as in the inputArray.
+
+function allLongestStrings(inputArray) {
+  var arr = [];
+
+  if( inputArray.length == 1){
+    return inputArray;
+  }
+  var longestWord = inputArray.sort(function(a, b) {
+   return b.length - a.length;
+  });
+  var lengthOfLongestWord = longestWord[0].length;//returns length of longest word
+
+
+  for(var i = 0; i < longestWord.length; i++){
+   if(longestWord[i].length == lengthOfLongestWord){
+     newarr.push(longestWord[i]);
+   }
+  }
+
+  return arr;
+  }
